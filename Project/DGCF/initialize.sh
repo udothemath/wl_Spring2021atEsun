2e468304d9d0d5ded908e33ce39bfb162a6942fc
Wei Le <r09946023@ntu.edu.tw>
2021-06-24 14:25:24 +0800

    Add files via upload

--- Project/DGCF/initialize.sh
+++ Project/DGCF/initialize.sh
@@ -5,9 +5,12 @@ mkdir data/
 mkdir results/
 
 mkdir saved_models/
-mkdir saved_models/reddit/
-mkdir saved_models/wikipedia/
-mkdir saved_models/mooc/
-mkdir saved_models/lastfm/
+#mkdir saved_models/reddit/
+#mkdir saved_models/wikipedia/
+#mkdir saved_models/mooc/
+#mkdir saved_models/lastfm/
+mkdir saved_models/Esun_datatime
+mkdir saved_models/Esun_week
+mkdir saved_models/Esun_weektime
 
 
